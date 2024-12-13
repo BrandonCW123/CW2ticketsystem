59c9db2bb04a7dc25ebf71b5e94839f819653e57
BrandonCW123 <s4400891@lsbu.ac.uk>
2024-12-13 15:03:09 +0000

    Third commit

--- CW2.py
+++ CW2.py
@@ -1,1 +1,11 @@
 print("Welcome to online ticket shop!")
+
+def main():
+    
+    DESTINATIONS = {
+        1: ("London", 10.15),
+        2: ("Manchester", 22.50), 
+        3: ("Birmingham", 21.40)
+    }
+
+    
