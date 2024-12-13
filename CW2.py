e5b792cfcee4bca4da242c38ec64e497a38bf58b
BrandonCW123 <s4400891@lsbu.ac.uk>
2024-12-13 15:05:01 +0000

    Fourth commit

--- CW2.py
+++ CW2.py
@@ -8,4 +8,15 @@ def main():
         3: ("Birmingham", 21.40)
     }
 
+    user_name = input("Please enter you name: ")
+
+    print("\nList of Destinations:")
+    for num, (user_destination, price) in DESTINATIONS.items():
+        print(f"{num}. {user_destination}: £{price:.2f}")
     
+    choice = int(input("\nPlease select your destination: "))
+    user_destination, price = DESTINATIONS[choice]
+    
+    tickets = int(input("How many ticket(s) would you like to purchase?: "))
+
+    user_total_price = price * tickets
